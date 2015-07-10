675e80073913a97a71c676ccfab243f9a1257497
benli0602 <s9472099@gmail.com>
2015-07-10 14:53:31 +0800

    FirstweekWork

---
+++ MVCProject/Controllers/ReportController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCProject.Models;
+
+namespace MVCProject.Controllers
+{
+    public class ReportController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View(db.報表.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
